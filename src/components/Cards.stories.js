84619c59ffcdf0ba4c48f0c7baff523ab8529534
Morten Kolbe <kolbe.mor@gmail.com>
2021-10-08 13:04:26 +0200

    create Cards.stories.js

---
+++ src/components/Cards.stories.js
@@ -0,0 +1,36 @@
+import Card from "./Card"
+
+export default {
+  title: "Component/Card",
+  component: Card,
+}
+
+const Template = (args) => <Card {...args} />
+
+export const WithAuthor = Template.bind({})
+WithAuthor.args = {
+  title: "Node.js",
+  text: "What is node.js and how to use it?",
+  author: "John Doe",
+}
+
+export const WithoutAuthor = Template.bind({})
+WithoutAuthor.args = {
+  title: "Node.js",
+  text: "What is node.js and how to use it?",
+}
+
+export const WithColor = Template.bind({})
+WithColor.args = {
+  title: "Node.js",
+  text: "What is node.js and how to use it?",
+  author: "John Doe",
+  color: "green",
+}
+
+export const WithoutColor = Template.bind({})
+WithoutColor.args = {
+  title: "Node.js",
+  text: "What is node.js and how to use it?",
+  author: "John Doe",
+}
